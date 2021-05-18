1dc3210fcd48935049568e76d1c17f322d449b38
safi <sh4773@nyu.edu>
2021-05-17 20:27:53 -0400

    initial project commit, parts 1-3 and 5 done

---
+++ application/Assignment4App/GiftcardSite/app/src/main/java/com/example/giftcardsite/ProductScrollingActivity.kt
@@ -0,0 +1,78 @@
+package com.example.giftcardsite
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.widget.Button
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.giftcardsite.api.model.*
+import com.example.giftcardsite.api.service.ProductInterface
+import com.example.giftcardsite.api.service.UserInfo
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ProductScrollingActivity : AppCompatActivity() {
+    var loggedInUser: User? = null
+    private var lastEvent : String? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
+        loggedInUser = intent.getParcelableExtra<User>("User")
+        findViewById<Button>(R.id.view_cards_button).setOnClickListener{
+            val intent = Intent(this, CardScrollingActivity::class.java).apply {
+                putExtra("User", loggedInUser)
+            }
+            startActivity(intent)
+        }
+        //var productList: List<Product?>? = null
+        var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsecclass.report").addConverterFactory(
+                GsonConverterFactory.create())
+        var retrofit: Retrofit = builder.build()
+        var client: ProductInterface = retrofit.create(ProductInterface::class.java)
+        val outerContext = this
+        var manager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = manager
+        client.getAllProducts()?.enqueue(object :
+                Callback<List<Product?>?> {
+            override fun onFailure(call: Call<List<Product?>?>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<List<Product?>?>, response: Response<List<Product?>?>) {
+                if (!response.isSuccessful) {
+                }
+                var productListInternal = response.body()
+                if (productListInternal == null) {
+                } else {
+                    recyclerView.adapter = RecyclerViewAdapter(outerContext, productListInternal, loggedInUser)
+                }
+            }
+        })
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+
+
+}
---
+++ application/Assignment4App/GiftcardSite/app/src/main/java/com/example/giftcardsite/api/service/UserInfo.kt
@@ -0,0 +1,12 @@
+package com.example.giftcardsite.api.service
+
+import com.example.giftcardsite.api.model.User
+import com.example.giftcardsite.api.model.UserInfoContainer
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface UserInfo {
+    // emptied out of all API calls and post attempt to API
+}
